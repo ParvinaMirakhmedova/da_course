da0587bfaf85902eb5aa6494edd977470c2c3cac
Parvina <mirakhmedova89@gmail.com>
2025-03-15 12:58:00 +0500

    add info

---
+++ Django.py
@@ -0,0 +1,28 @@
+# print("2+2")
+# print("2-2")
+# print("2*2")
+# print("2/2")
+# print("2//2")
+
+# is_sunny = False
+# if is_sunny:
+#     print("I will play outside")
+# else:
+#     print("I wouldn't play outside")
+
+# a = int(input("Введите число:"))
+# if a>0:
+#     print("Positive")
+# elif a<0:
+#     print("Negative")
+# else:
+#     print("Zero")
+
+
+
+def say_hello(n):
+    for i in range(n):
+        print("Hello,world")
+
+n = int(input())
+say_hello(n)
